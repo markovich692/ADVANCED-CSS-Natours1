ece40077055867762d65ef71c21edf81fd9fac60
Marc Diallo <mark.devguru@outlook.com>
2025-04-29 22:04:15 -0500

    starts the complimentary DNA kata

--- test.js
+++ test.js
@@ -1,66 +1,11 @@
-// You are back at your newly acquired decrypting job for the secret organization when a new assignment
-//  comes in. Apparently the enemy has been communicating using a device they call "The Mirror".
-// It is a rudimentary device with encrypts the message by switching its letter
-// with its mirror opposite (A => Z), (B => Y), (C => X) etc.
+// Description:
+// Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries
+// the "instructions" for the development and functioning of living organisms.
+// If you want to know more: http://en.wikipedia.org/wiki/DNA
+// In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives
+//  one side of the DNA (string, except for Haskell); you need to return the other complementary side.
+//  DNA strand is never empty or there is no DNA at all (again, except for Haskell).
 
-// Your job is to build a method called "mirror" which will decrypt the messages.
-//  Resulting messages will be in lowercase.
+// More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
 
-// To add more secrecy, you are to accept a second optional parameter,
-// telling you which letters or characters are to be reversed; if it is
-//  not given, consider the whole alphabet as a default.
-
-// To make it a bit more clear: e.g. in case of "abcdefgh" as the second optional parameter,
-//  you replace "a" with "h", "b" with "g" etc. .
-
-// function mirror(code) {
-//   const splitInput = code.toLowerCase().split("");
-//   let arr = [];
-// prettier-ignore
-//   const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-//   const reversedAlphabet = [...alphabet].reverse(); // use a copy so original isn't reversed
-
-//   splitInput.forEach((letter) => {
-//     const indexGet = alphabet.findIndex((letterAlph) => {
-//       return letterAlph === letter;
-//     });
-
-//     reversedAlphabet[indexGet]
-//       ? arr.push(reversedAlphabet[indexGet])
-//       : arr.push(" ");
-//   });
-
-//   return [arr[0].toUpperCase(), ...arr.slice(1)].join("");
-// }
-
-// mirror("Welcome home");
-
-///////////////////////////////////////////////////////////////////////////
-// Return the number (count) of vowels in the given string.
-
-// We will consider a, e, i, o, u as vowels for this Kata (but not y).
-
-// The input string will only consist of lower case letters and/or spaces.
-
-const vowelsCount = function(str){
-
-let vowelsNumber=0;
-const splitString = str.toLowerCase().split('');
-splitString.forEach((letter)=>{
-
-const includesVowels = 'aeiou'.includes(letter) ? 1 : 0;
-
-
-
-vowelsNumber = vowelsNumber + includesVowels;
-
-
-
-}); 
-
-
-return vowelsNumber;
-
-};
-
-vowelsCount("abracadabra");
+// Example: (input --> output)
