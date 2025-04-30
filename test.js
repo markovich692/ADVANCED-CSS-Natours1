02ac7236b320325b6c7bb3046dc5735c994c1ba5
Marc Diallo <mark.devguru@outlook.com>
2025-04-29 21:33:53 -0500

    tries to solve the challenge but will come back to it

--- test.js
+++ test.js
@@ -1,13 +1,36 @@
-// Let's play! You have to return which player won! In case of a draw return Draw!.
+// You are back at your newly acquired decrypting job for the secret organization when a new assignment
+//  comes in. Apparently the enemy has been communicating using a device they call "The Mirror".
+// It is a rudimentary device with encrypts the message by switching its letter
+// with its mirror opposite (A => Z), (B => Y), (C => X) etc.
 
-// Examples(Input1, Input2 --> Output):
+// Your job is to build a method called "mirror" which will decrypt the messages.
+//  Resulting messages will be in lowercase.
 
-// "scissors", "paper" --> "Player 1 won!"
-// "scissors", "rock" --> "Player 2 won!"
-// "paper", "paper" --> "Draw!"
+// To add more secrecy, you are to accept a second optional parameter,
+// telling you which letters or characters are to be reversed; if it is
+//  not given, consider the whole alphabet as a default.
 
-const whoWon = function (input1, input2) {
-  if (input1 === "scissors" && input2 === "paper") return "Player 1 won";
-  if (input1 === "scissors" && input2 === "rock") return "Player 2 won";
-  if (input1 === input2) return "Draw";
-};
+// To make it a bit more clear: e.g. in case of "abcdefgh" as the second optional parameter,
+//  you replace "a" with "h", "b" with "g" etc. .
+
+function mirror(code) {
+  const splitInput = code.toLowerCase().split("");
+  let arr = [];
+  // prettier-ignore
+  const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  const reversedAlphabet = [...alphabet].reverse(); // use a copy so original isn't reversed
+
+  splitInput.forEach((letter) => {
+    const indexGet = alphabet.findIndex((letterAlph) => {
+      return letterAlph === letter;
+    });
+
+    reversedAlphabet[indexGet]
+      ? arr.push(reversedAlphabet[indexGet])
+      : arr.push(" ");
+  });
+
+  return [arr[0].toUpperCase(), ...arr.slice(1)].join("");
+}
+
+mirror("Welcome home");
