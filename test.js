db463838a199947aaa5cd58c969f3a6af21dce84
Marc Diallo <mark.devguru@outlook.com>
2025-05-01 21:17:59 -0500

    initializes npm, installs sass as a dev dependency, adds node_mpdules to .gitignore

---
+++ test.js
@@ -0,0 +1,25 @@
+// Write a function that returns a sequence (index begins with 1)
+// of all the even characters from a string. If the string is smaller
+//  than two characters or longer than 100 characters,
+//  the function should return "invalid string".
+
+const sequence = function (string) {
+  const strSplit = string.split("");
+
+  if (strSplit.length === 1 || strSplit.length > 100) return "invalid string";
+
+  const newStringArray = strSplit
+    .map((letter, i) => {
+      const index = i + 1;
+      return index % 2 === 0 ? letter : " ";
+    })
+    .filter((letter) => letter !== " ");
+
+  return newStringArray;
+};
+
+sequence("abcdefghijklm");
+// sequence("a");
+
+// "abcdefghijklm" --> ["b", "d", "f", "h", "j", "l"]
+// "a"             --> "invalid string"
